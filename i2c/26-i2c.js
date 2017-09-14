6ac115c414cc74313cc2ece86b7486b9ec975551
Niels v/d Spek <nielsnl68@gmail.com>
2017-09-14 14:11:41 +0200

    Switching for i2c to i2c-bus library.
    The other looks robuster and better for asynchrone instructions.

--- i2c/26-i2c.js
+++ i2c/26-i2c.js
@@ -1,126 +1,24 @@
 module.exports = function(RED) {
     "use strict";
-    var I2C = require("i2c");
+    var I2C = require("i2c-bus");
 
-    // The Server Definition - this opens (and closes) the connection
+    // The Server DefieventTasks.DispatchNodesnition - this opens (and closes) the connection
     function I2CServerNode(n) {
         RED.nodes.createNode(this, n);
         this.device = n.device || "/dev/I2C-1";
-        this.address = n.address || 0x18;
         this.port = null;
         this.on("close", function() {
             if (this.port != null) {
                 //     this.port.disconnect();
+				this.port.closeSync();
+				this.port = null;
+				var globalContext = this.context().global;
+				globalContext.set("i2cport", null); 
             }
         });
     }
     RED.nodes.registerType("i2c-device", I2CServerNode);
 
-
-    // The Input Node
-    function I2CInNode(n) {
-        RED.nodes.createNode(this, n);
-        this.i2cdevice = n.i2cdevice;
-        this.serverConfig = RED.nodes.getNode(this.i2cdevice);
-        this.address = n.address;
-        this.command = n.command;
-        this.count = n.count;
-        var node = this;
-        if (node.serverConfig.port === null) {
-            node.log("CONNECT: " + node.serverConfig.device);
-            node.serverConfig.port = new I2C(parseInt(this.serverConfig.address), {
-                device: node.serverConfig.device
-            });
-        }
-        node.port = node.serverConfig.port;
-        node.on("input", function(msg) {
-            var address = node.address || msg.address || this.serverConfig.address;
-            var command = node.command || msg.command;
-            node.port.setAddress(parseInt(address));
-            node.port.readBytes(parseInt(command), node.count, function(err, res) {
-                if (err) {
-                    node.error(err);
-                } else {
-                    var payload;
-                    if (Buffer.isBuffer(res) && node.count == 1) {
-                        payload = res[0];
-                    } else {
-                        payload = res;
-                    }
-                    // msg.address = address;
-                    // msg.command = command;
-                    // msg.payload = payload;
-
-                    node.send({
-                        address: address,
-                        command: command,
-                        payload: payload
-                    });
-                }
-            });
-        });
-
-        node.on("close", function() {
-            //   node.port.free();
-        });
-    }
-    RED.nodes.registerType("i2c in", I2CInNode);
-
-
-    // The Output Node
-    function I2COutNode(n) {
-        RED.nodes.createNode(this, n);
-        this.i2cdevice = n.i2cdevice;
-        this.serverConfig = RED.nodes.getNode(this.i2cdevice);
-        this.address = parseInt(n.address);
-        this.command = parseInt(n.command);
-        this.count = parseInt(n.count);
-        this.payload = n.payload;
-        var node = this;
- 
-        if (node.serverConfig.port === null) {
-            node.log("CONNECT: " + node.serverConfig.device);
-   
-            node.serverConfig.port = new I2C(parseInt(this.serverConfig.address), {
-                device: node.serverConfig.device
-            });
-        }
-
-        node.port = node.serverConfig.port;
-
-        node.on("input", function(msg) {
-            var i2caddress = node.address || msg.address || this.serverConfig.address;
-            var i2ccommand = node.command || msg.command;
-
-            node.port.setAddress(i2caddress);
-            var payload = node.payload || msg.payload;
-            if (payload == null || node.count == 0) {
-		node.port.writeByte(parseInt(i2ccommand),  function(err) {
-                    if (err) node.error(err);
-                });
-	    } else if (!isNaN(payload)) {
-		var data = payload;
-
-		payload = Buffer.allocUnsafe(node.count);
-		payload.writeIntLE(data, 0, node.count, true);
-
-	    } else if ((typeof payload === "string") || Array.isArray(payload)) {
-		payload = Buffer.from(payload);
-	    }
-            if (payload.count > 32) {
-                node.error("Too many elements in array to write to I2C");
-            } else {
-                node.port.writeBytes(parseInt(i2ccommand), payload, function(err) {
-                    if (err) node.error(err);
-                });
-            }
-        });
-
-        node.on("close", function() {
-            //     node.port.free();
-        });
-    }
-    RED.nodes.registerType("i2c out", I2COutNode);
 
     // The Output Node
     function I2CScanNode(n) {
@@ -130,10 +28,9 @@ module.exports = function(RED) {
         var node = this;
         if (node.serverConfig.port === null) {
             node.log("CONNECT: " + node.serverConfig.device);
-
-            node.serverConfig.port = new I2C(parseInt(this.serverConfig.address), {
-                device: node.serverConfig.device
-            });
+            node.serverConfig.port = I2C.openSync( 1 );
+			var globalContext = this.context().global;
+			globalContext.set("i2cport", node.serverConfig.port); 
 
         }
         node.port = node.serverConfig.port;
@@ -161,5 +58,157 @@ module.exports = function(RED) {
             //   node.port.free();
         });
     }
-    RED.nodes.registerType("i2c scan", I2CScanNode);
+    RED.nodes.registerType("i2c scan", I2CScanNode);	
+	
+    // The Input Node
+    function I2CInNode(n) {
+        RED.nodes.createNode(this, n);
+        this.i2cdevice = n.i2cdevice;
+        this.serverConfig = RED.nodes.getNode(this.i2cdevice);
+        this.address = n.address;
+        this.command = n.command;
+        this.count = n.count;
+        var node = this;
+        if (node.serverConfig.port === null) {
+            node.log("CONNECT: " + node.serverConfig.device);
+            node.serverConfig.port = I2C.openSync( 1 );
+			var globalContext = this.context().global;
+			globalContext.set("i2cport", node.serverConfig.port); 			
+        }
+		this.status({});
+        node.port = node.serverConfig.port;
+        node.on("input", function(msg) {
+            var address = node.address || msg.address ;
+            var command = node.command || msg.command ;
+			address = parseInt(address);
+			command = parseInt(command);
+			var buffcount = parseInt(node.count);
+			if ((!address) || (!command) || isNaN(address) || isNaN(command) ) {
+			  this.status({fill:"red",shape:"ring",text:"Address value is missing or incorrect"});	
+			  return;
+			} else if ((!address) || (!command) || isNaN(address) || isNaN(command) ) {
+			  this.status({fill:"red",shape:"ring",text:"Command value is missing or incorrect"});	
+			  return;
+			} else if ((!buffcount) || isNaN(buffcount) ) {
+			  this.status({fill:"red",shape:"ring",text:"Read bytes value is missing or incorrect"});	
+			  return;
+			} else {
+				this.status({});
+			}
+
+			var buffer = new Buffer(buffcount);	
+            node.port.readI2cBlock(address, command, buffcount, buffer, function(err, size, res) {
+                if (err) {
+                    node.error(err);
+                } else {
+                    var payload;
+                    if (node.count == 1) {
+                        payload = res[0];
+                    } else {
+                        payload = res;
+                    }
+					msg = Object.assign({}, msg);
+                 //   node.log('log returned data'+  JSON.stringify([buffer.length, buffer, buffer.toString("utf-8")]));
+                  //  node.log('log returned data'+  JSON.stringify([size, res.length, res, res.toString("utf-8")]));
+					msg.address = address;
+                    msg.command = command;
+                    msg.payload = payload;
+					msg.buffer  = buffer;
+					msg.returned  = res;					
+					msg.size  = size;					
+                    node.send(msg);
+                }
+            });
+        });
+
+        node.on("close", function() {
+            //   node.port.free();
+        });
+    }
+    RED.nodes.registerType("i2c in", I2CInNode);
+
+
+    // The Output Node
+    function I2COutNode(n) {
+        RED.nodes.createNode(this, n);
+        this.i2cdevice = n.i2cdevice;
+        this.serverConfig = RED.nodes.getNode(this.i2cdevice);
+        this.address = parseInt(n.address);
+        this.command = parseInt(n.command);
+        this.count = parseInt(n.count);
+        this.payload = n.payload;
+        this.payloadType = n.payloadType;
+        var node = this;
+ 
+        if (node.serverConfig.port === null) {
+            node.log("CONNECT: " + node.serverConfig.device);
+            node.serverConfig.port = I2C.openSync( 1 );
+			var globalContext = this.context().global;
+			globalContext.set("i2cport", node.serverConfig.port); 			
+		}
+
+        node.port = node.serverConfig.port;
+
+        node.on("input", function(msg) {
+			var myPayload;
+			var address = node.address || msg.address;
+            var command = node.command || msg.command;
+			address = parseInt(address);
+			command = parseInt(command);
+			var buffcount = parseInt(node.count);
+			if ((!address) || (!command) || isNaN(address) || isNaN(command) ) {
+			  this.status({fill:"red",shape:"ring",text:"Address value is missing or incorrect"});	
+			  return;
+			} else if ((!address) || (!command) || isNaN(address) || isNaN(command) ) {
+			  this.status({fill:"red",shape:"ring",text:"Command value is missing or incorrect"});	
+			  return;
+			} else if ((!buffcount) || isNaN(buffcount) ) {
+			  this.status({fill:"red",shape:"ring",text:"Send bytes value is missing or incorrect"});	
+			  return;
+			} else {
+				this.status({});
+			}
+
+			
+			try {
+                if (this.payloadType == null) {
+                    myPayload = this.payload;
+                } else if (this.payloadType == 'none') {
+                    myPayload = null;
+                } else {
+                    myPayload = RED.util.evaluateNodeProperty(this.payload, this.payloadType, this,msg);
+                }			
+				
+				if (myPayload == null || node.count == 0) {
+					node.port.writeByte(address, command,  function(err) {
+						if (err) node.error(err, msg);
+					});
+				} else if (!isNaN(myPayload)) {
+					var data = myPayload;
+					myPayload = Buffer.allocUnsafe(node.count);
+					myPayload.writeIntLE(data, 0, node.count, true);
+					
+				} else if (typeof myPayload === "string" || Array.isArray(myPayload)) {
+					myPayload = Buffer.from(myPayload);
+				}
+				if (myPayload.length > 32) {
+					node.error("Too many bytes to write to I2C");
+				} else {
+				//	node.log('log write data'+  JSON.stringify([address, command, myPayload.length, myPayload, myPayload.toString("utf-8")]));
+
+					node.port.writeI2cBlock(address, command, myPayload.length, myPayload, function(err) {
+						if (err) node.error(err);
+					});
+				}
+			
+            } catch(err) {
+                this.error(err,msg);
+            }			
+        });
+
+        node.on("close", function() {
+            //     node.port.free();
+        });
+    }
+    RED.nodes.registerType("i2c out", I2COutNode);
 }
